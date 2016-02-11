3557f941b44e2953f34f27e42be9c36431658558
Miles Dickinson <miles.dickinson92@gmail.com>
2016-02-10 20:31:41 -0500

    Finished assignment 4

--- assignment-4/js/contact.js
+++ assignment-4/js/contact.js
@@ -1,11 +1,11 @@
-(function() {
+$(document).ready(function() {
     'use strict';
 
     function submitContactForm(evt) {
         evt.preventDefault();
 
         var emailRegex = /[A-Z0-9]+@[A-Z0-9]+\.[A-Z]{2,4}/gi;
-        var phoneRegex = /\([0-9]{3}\)-[0-9]{3}-[0-9]{4}/g;
+        var phoneRegex = /[0-9]{3}-[0-9]{3}-[0-9]{4}/g;
 
         var emailInput = document.querySelector('input[name=email]').value;
         var phoneInput = document.querySelector('input[name=phone]').value;
@@ -14,15 +14,15 @@
         var validPhone = phoneRegex.test(phoneInput);
 
         if (!validEmail) {
-            document.querySelector('label[for=email] span').className = 'invalid-email';
+            $('label[for=email] span').addClass('invalid-email');
         } else {
-            document.querySelector('label[for=email] span').className = '';
+            $('label[for=email] span').removeClass('invalid-email');
         }
 
         if (!validPhone) {
-            document.querySelector('label[for=phone] span').className = 'invalid-phone';
+            $('label[for=phone] span').addClass('invalid-phone');
         } else {
-            document.querySelector('label[for=phone] span').className = '';
+            $('label[for=phone] span').removeClass('invalid-phone');
         }
     }
 
@@ -31,12 +31,12 @@
         $('.' + selectedMethod).addClass('visible');
     }
 
-    document.querySelector('#contact-method').addEventListener('change', function onContactMethodChange() {
+    $('.contact-method').change(function onContactMethodChange() {
         if (this.selectedIndex > -1) {
             displayContactMethod(this.options[this.selectedIndex].value);
         }
-    }, false);
+    });
 
-    document.querySelector('button[type=submit]').addEventListener('click', submitContactForm, false);
+    $('#submit-contact-form').click(submitContactForm);
 
-}());
+});
